bb3203b5e2ee1b0bf412a5846485dfb085662997
bobzz-zone <bobzz.zone@gmail.com>
2020-07-14 01:37:18 +0700

    Update custom_method.py

--- my_account/my_account/doctype/custom_method.py
+++ my_account/my_account/doctype/custom_method.py
@@ -22,45 +22,45 @@ def get_enabled_users():
 	return frappe.db.sql("""select CAST(COUNT(*) AS CHAR) from `tabUser`
 		where enabled = 1 and name not in ("Administrator","Guest") """)[0][0]
 
-@frappe.whitelist()
-def validate_user_quota(doc,method):
-	if doc.name in ["Administrator","Guest"]:
-		return
-	desktop = frappe.db.sql("SELECT defvalue FROM `tabDefaultValue` WHERE defkey = 'desktop:home_page'")[0][0]
-	is_created = 1
-	try:
-		subdomain = frappe.local.site.split(".")[0]
-	except:
-		frappe.throw("Domain not found")
-	if subdomain == "reg":
-		is_created = frappe.db.get_value("Master Subdomain",subdomain,"is_created")
-	# print(desktop)
-	# print("is_created = {}".format(is_created))
-	# print("site = {}".format(subdomain))
-	quota = 0
-	if desktop != "setup-wizard" and is_created == 1:
+# @frappe.whitelist()
+# def validate_user_quota(doc,method):
+# 	if doc.name in ["Administrator","Guest"]:
+# 		return
+# 	desktop = frappe.db.sql("SELECT defvalue FROM `tabDefaultValue` WHERE defkey = 'desktop:home_page'")[0][0]
+# 	is_created = 1
+# 	try:
+# 		subdomain = frappe.local.site.split(".")[0]
+# 	except:
+# 		frappe.throw("Domain not found")
+# 	if subdomain == "reg":
+# 		is_created = frappe.db.get_value("Master Subdomain",subdomain,"is_created")
+# 	# print(desktop)
+# 	# print("is_created = {}".format(is_created))
+# 	# print("site = {}".format(subdomain))
+# 	quota = 0
+# 	if desktop != "setup-wizard" and is_created == 1:
 		
-		quota = subprocess.check_output("""bench --site reg.solubis.id execute my_account.my_account.doctype.api_data.get_current_quota --args "['{}']" """.format(str(subdomain)),shell=True,universal_newlines=True)
-		# print("quota = {}".format(float(quota)))
-		enabled_users = get_enabled_users()
+# 		quota = subprocess.check_output("""bench --site reg.solubis.id execute my_account.my_account.doctype.api_data.get_current_quota --args "['{}']" """.format(str(subdomain)),shell=True,universal_newlines=True)
+# 		# print("quota = {}".format(float(quota)))
+# 		enabled_users = get_enabled_users()
 
-		# print("enabled = {}".format(enabled_users))
-		# print("quota = {}".format(quota))
-		if flt(quota) < flt(enabled_users)-1:
-			# if user created delete user
-			usr_enabled = frappe.get_doc("User",doc.name)
-			if usr_enabled.enabled == 1:
-				frappe.db.sql("UPDATE `tabUser` set enabled = 0 where name = '{}'".format(doc.name))
-			frappe.throw("Max enabled users reached for {} ({}/{})".format(frappe.local.site,flt(quota),flt(enabled_users)))
-	# custom saas andy for solubis check for update password
-	if doc.new_password:
-		# check if my account installed
-		my_acc = frappe.db.sql('SELECT * FROM `tabDefaultValue` WHERE defkey = "installed_apps" AND defvalue LIKE CONCAT("%","my_account","%");')
-		usrexist = frappe.db.sql('SELECT * FROM `tabUser` WHERE name = "{}" and name not in ("Administrator","Guest")'.format(doc.name),as_dict=1)
-		if len(my_acc) > 0 and len(usrexist) > 0:
-			pu = frappe.get_doc("Purchase User", user)
-			pu.current_password = pwd
-			pu.save(ignore_permissions=True)
+# 		# print("enabled = {}".format(enabled_users))
+# 		# print("quota = {}".format(quota))
+# 		if flt(quota) < flt(enabled_users)-1:
+# 			# if user created delete user
+# 			usr_enabled = frappe.get_doc("User",doc.name)
+# 			if usr_enabled.enabled == 1:
+# 				frappe.db.sql("UPDATE `tabUser` set enabled = 0 where name = '{}'".format(doc.name))
+# 			frappe.throw("Max enabled users reached for {} ({}/{})".format(frappe.local.site,flt(quota),flt(enabled_users)))
+# 	# custom saas andy for solubis check for update password
+# 	if doc.new_password:
+# 		# check if my account installed
+# 		my_acc = frappe.db.sql('SELECT * FROM `tabDefaultValue` WHERE defkey = "installed_apps" AND defvalue LIKE CONCAT("%","my_account","%");')
+# 		usrexist = frappe.db.sql('SELECT * FROM `tabUser` WHERE name = "{}" and name not in ("Administrator","Guest")'.format(doc.name),as_dict=1)
+# 		if len(my_acc) > 0 and len(usrexist) > 0:
+# 			pu = frappe.get_doc("Purchase User", user)
+# 			pu.current_password = pwd
+# 			pu.save(ignore_permissions=True)
 
 @frappe.whitelist(allow_guest=True)
 def reduce_days_active_user():
